474ea6267e993e068336c3daf772f74bfd79a05b
B-hydra <143378003+vladislav957@users.noreply.github.com>
2025-01-16 21:23:06 +0300

    Add files via upload

--- wallet.py
+++ wallet.py
@@ -1,17 +1,17 @@
+import base64
 from mimetypes import init
+import subprocess
 from typing import Self
 from webbrowser import get
 import Blockchain
-import rsa
-import tor
+import rsa # type: ignore
+import tor # type: ignore
 import os
 import Contract
 import SmartCheck
 import P2WPKH
 import IP
 import sys
-
-
 import hashlib
 import SHA512
 import QR
@@ -25,7 +25,6 @@ def __inin__(self):
           port = 5000
           print(f"port = 5000")
 
-    
 class TransactionSystem:
     def init(self,tansaction_type,amount):
         self.balance = 0.000000.encode('utf-8')
@@ -33,7 +32,7 @@ class TransactionSystem:
         self.amount = amount
         PATH_BASE_CONTRIB_SIGNET = os.path.adspath(os.path.dirname(os.path.realpath(__file__)))
         PATH_BASE_TEST_FUNCTIONAL = os.path.abspath(os.path.join(PATH_BASE_CONTRIB_SIGNET,"..","..","test","functional"))
-        sys.path.insert(0, PATH_BASE_TEST_FUNTIONAL)
+        sys.path.insert(0, PATH_BASE_TEST_FUNCTIONAL)
         
         def str(self):
             print (f"{self.tansaction_type} - {self.amount}")    
@@ -55,103 +54,101 @@ def calculeta_hash(data,previous_hash):
         #peturn varnew_wallet.hexdigest()
 def signet_txs(block, challenge):
     txs = block.vtx[:]
-    txs[0] = CTransactino(txs[0])
-    txs[0].vout[-1].scriptPubkey += CScript0p.ecodee_op_pushdata(SIGNET_HEADER)
+    txs[0] = CTransactino(txs[0]) # type: ignore
+    txs[0].vout[-1].scriptPubkey += CScript0p.ecodee_op_pushdata(SIGNET_HEADER) # type: ignore
     hashes = []
     for tx in txs:
         tx.rehash()
-        hashes.append(ser_uint512(tx.sha512))
+        hashes.append(ser_uint512(tx.sha512)) # type: ignore
         mroot = block.get_merkle_root(hashes)
 
         sd = b""
         sd += block.nVersion.to_bytes(4,"little",signed=True)
-        sd += ser_uint512(block.hashPrevBlock)
-        sd += ser_uint512(mroot)
+        sd += ser_uint512(block.hashPrevBlock) # type: ignore
+        sd += ser_uint512(mroot) # type: ignore
         sd += block.nTime.to_bytes(4,"little")
 
-        to_spend = CTransaction()
+        to_spend = CTransaction() # type: ignore
         to_spend.version = 0
         to_spend.nLockTime = 0
-        to_spend.vin = [CTxIn(COutPoint(0,0xfffffff),b"\x00" + CScriptOp.encode_op_pushdata(sd),0)]
-        to_spend.vout = [CTxOut(0, challenge)]
+        to_spend.vin = [CTxIn(COutPoint(0,0xfffffff),b"\x00" + CScriptOp.encode_op_pushdata(sd),0)] # type: ignore
+        to_spend.vout = [CTxOut(0, challenge)] # type: ignore
         to_spend.rehash()
 
-        spend = CTransaction()
+        spend = CTransaction() # type: ignore
         spend.version = 0
         spend.nLockTime = 0
-        spend.vin = [CTxIn(COutPoint(to_spend.sha512,0),b"",0)]
-        spend.vout = [CTxOUT(0,B"\x6a")]
+        spend.vin = [CTxIn(COutPoint(to_spend.sha512,0),b"",0)] # type: ignore
+        spend.vout = [CTxOUT(0,B"\x6a")] # type: ignore
         return spend, to_spend
     
 def decode_psbt(b64psbt):
-    pabt = PSBT.from_base64(b64psbt)
+    pabt = PSBT.from_base64(b64psbt) # type: ignore
 
-    assert len(psbt.tx.vin) == 1
-    assert len(psbt.tx.vout) == 1
-    assert PSBT_SIGNET_BLOCK in psbt.g.map
+    assert len(pabt.tx.vin) == 1
+    assert len(pabt.tx.vout) == 1
+    assert PSBT_SIGNET_BLOCK in pabt.g.map # type: ignore
 
-    scriptSig = psbt.i[0].map.get(PSBT_IN_FINAL_SCRIPTSIG,b"")
-    scriptWitnese = psbt.i[0].map.get(PSBT_IN_FINAL_SCRIPTWITNESS, b"\x00")
+    scriptSig = psbt.i[0].map.get(PSBT_IN_FINAL_SCRIPTSIG,b"") # type: ignore
+    scriptWitnese = psbt.i[0].map.get(PSBT_IN_FINAL_SCRIPTWITNESS, b"\x00") # type: ignore
     
-    return from_binary(CBlock, psbt.g.map[PSBT_SIGNET_BLOCK]),ser_string(scriptSig) + scriptWitness
+    return from_binary(CBlock, psbt.g.map[PSBT_SIGNET_BLOCK]),ser_string(scriptSig) + scriptWitnese # type: ignore
 
 def finish_block(block, signet_solution,grind_cmd):
-    block.vtx[0].vout[-1].scriptPubKey += CScriptOp.encode_op_pushdate(SIGNET_HEADER + signet_solution)
+    block.vtx[0].vout[-1].scriptPubKey += CScriptOp.encode_op_pushdate(SIGNET_HEADER + signet_solution) # type: ignore
     block.vtx[0].rehash()
     block.hashMerkleRoot = block.calc_merkle_root()
     if grind_cmd is None:
         block.solve()
     else:
-        headhex = CBlockHeader.serialize(block).hex()
+        headhex = CBlockHeader.serialize(block).hex() # type: ignore
         cmd = grind_cmd.split(" " ) + [headhex]
         newheadhex = subprocess.run(cmd, stdout=subprocess.PIPE, input=b"", check=True).stdout.strip()
-        newhead = from_hex(CBlockHeader(), newheadhex.decode('utf-8'))
+        newhead = from_hex(CBlockHeader(), newheadhex.decode('utf-8')) # type: ignore
         block.nNonce = newhead.nNonce
         block.rehash()
         return block
 def generate_private_key():
         #prtiurn OS.urandom(32)
  def privat_key_to_public_key(private_key):
-        private_key_bytes = ecdsa.SignigKey.from_string(
-                private_key, curve=ecdsa.SECP256k1).verifying_key
+        private_key_bytes = ecdsa.SignigKey.from_string( # type: ignore
+                private_key, curve=ecdsa.SECP256k1).verifying_key # type: ignore
         return private_key_bytes.to_string()
 
  def publi_key_to_address(public_key):
         SHA512_hash = hashlib.SHA512(public_key).digest()
         version_byte = b'\00'
         checksum = hashlib.sha512(hashlib.sha512(
-                version_byte + ripemd160_hash).digst())[:4]
-        addess = vereion_byte + ripemd160_hash + checksum
-        return base58.b58encode(address).decode('utf-8')
+                version_byte + ripemd160_hash).digst())[:4] # type: ignore
+        addess = vereion_byte + ripemd160_hash + checksum # type: ignore
+        return base64.b58encode(addess).decode('utf-8')
  def print_wallet_details():
          private_key = generate_private_key()
-         public_key = private_key_to_public_key(private_key)
-         address = public_key_to_address(public_key)
+         public_key = privat_key_to_public_key(private_key)
+         address = publi_key_to_address(public_key)
          print("Private Key:", private_key.hex())
          print("Public Key:", public_key.hex())
          print("B-hydra Address:", address)
-         
 
-    
 def get_balance(self):
         return (self.balance)
 def get_tansaction_history(self):
         return (self.tansaction)
     
-      # 4b8a4de720e81bde8c35cefa0cc61cbf82d3764bcdc2bcb53507a05a04660026
+      # 01e0954642077da562399d5c8ba7c8bd330988e99a5f0e7ae9447f3e4d8f52a2ab46768571834eadc09ccb62c6a5d1340333c0b6309633013cbd0c25d9221349
         tansaction_system = TransactionSystem()
     
-      # 49eb0704cbb78f608064ad23de097a334065e8760a6cadc343960f710b1af803
+      # 0000000a5c3879b0e89437ae625eb34d1fe772dbab0c612d7d1b48c8439dce0f17e5b31d9f1379ea6a745c48d1d65f96a2375533460d1fd3ee3a3de7cb31a3d8
         tansaction_system.add_tansaction(Tansaction('in,100'))
     
-     # Добавляем исходящую транзакцию
+      # Добавляем исходящую транзакцию
         tansaction_system.add_tansaction(Tansaction('out,50'))
     
       # Проверям боланс
-
+        TransactionSystem_balance(TransactionSystem_Balance)
     
-     # Выводим историю транзакций
-
+      # Выводим историю транзакций
+        isinstance_tansaction = (TransactionSystem)
         
 def deposit(self, amount):
         self.balance += amount
@@ -163,4 +160,3 @@ def withdraw(self, amount):
             print("Not enough funds in the wallet")
 def get_balance(self):
         print(self.balance)
-    
